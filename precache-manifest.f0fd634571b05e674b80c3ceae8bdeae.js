54a3074c464e5aa2176d74b498519b187698d20f
Fellipe Coelho <fontan.fellipe@gmail.com>
2019-12-14 16:07:47 -0300

    Updates

---
+++ precache-manifest.f0fd634571b05e674b80c3ceae8bdeae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de30d57f62c628fd83c7f6f5fd15c43",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "da20c57d050549a3f6af",
+    "url": "/robotfriends/static/css/2.81310d1a.chunk.css"
+  },
+  {
+    "revision": "0a1180c8a8be65acce46",
+    "url": "/robotfriends/static/css/main.d15e702f.chunk.css"
+  },
+  {
+    "revision": "da20c57d050549a3f6af",
+    "url": "/robotfriends/static/js/2.718eafd2.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/robotfriends/static/js/2.718eafd2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a1180c8a8be65acce46",
+    "url": "/robotfriends/static/js/main.65228fb5.chunk.js"
+  },
+  {
+    "revision": "20557a821ba7203c7a33",
+    "url": "/robotfriends/static/js/runtime-main.aef2ab28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/robotfriends/static/media/logo.5d5d9eef.svg"
+  }
+]);
